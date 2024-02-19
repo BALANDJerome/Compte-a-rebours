1e119d0304fb762a68246c8c4b15095bc5a62215
BALAND Jérome <baland.jerome@gmail.com>
2024-02-19 13:13:56 +0100

    ajout button reset

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let totalSeconds = ""
+let totalSeconds = 0
 let minutes = ""
 let seconds = ""
+let timeoutId
 const ring = () => {
   const audio = new Audio
   audio.src = "./Assets/ring.mp3"
@@ -9,13 +10,13 @@ const ring = () => {
 const display = () => {
   countdownDisplay.innerHTML = `<p>${minutes}:${(seconds < 10 ? "0" + seconds : seconds)}</p>`
 }
-const countdown = () => setTimeout(() => {
+const countdown = () => timeoutId = setTimeout(() => {
   minutes = Math.floor(totalSeconds / 60);
   seconds = totalSeconds % 60
   display()
   if (totalSeconds > 0) {
     totalSeconds--
-    console.log(seconds);
+    console.log(totalSeconds);
     countdown()
   } else {
     countdownDisplay.innerHTML = `<p>Finish</p>`
@@ -26,11 +27,20 @@ const countdown = () => setTimeout(() => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-  totalSeconds = choice.value * 60
-  minutes = Math.floor(totalSeconds / 60);
-  seconds = totalSeconds % 60
-  countdown()
-  totalSeconds--
-  display()
+  if (totalSeconds === 0) {
+    totalSeconds = choice.value * 60
+    minutes = Math.floor(totalSeconds / 60);
+    seconds = totalSeconds % 60
+    countdown()
+    totalSeconds--
+    display()
+    reset.innerHTML = `<button type="submit">RESET</button>`
+  }
+})
+reset.addEventListener("click", () => {
+  clearTimeout(timeoutId)
+  totalSeconds = 0
+  countdownDisplay.innerHTML = ``
+  reset.innerHTML = ``
 })
 
